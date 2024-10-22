c919066ee69d1a965a7bafea9f358b9c875e50a7
Prajwal267 <prajwalchola26@gmail.com>
2024-10-22 21:30:14 +0530

    first commit

---
+++ src/main/java/com/prajwaldev/HotelManager/dto/Response.java
@@ -0,0 +1,26 @@
+package com.prajwaldev.HotelManager.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class Response {
+    private int statusCode;
+    private String message;
+
+    private String token;
+    private String role;
+    private String expirationTime;
+    private String bookingConfirmationCode;
+
+    private UserDTO user;
+    private RoomDTO room;
+    private BookingDTO booking;
+    private List<UserDTO> userList;
+    private List<RoomDTO> roomList;
+    private List<BookingDTO> bookingList;
+
+}
